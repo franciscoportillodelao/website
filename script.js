e528c172c80e5e62700713e8373a5cdebf3be217
franciscoportillodelao <138137628+franciscoportillodelao@users.noreply.github.com>
2023-06-29 09:48:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var dButton = document.getElementById("default");
+var body = document.body;
+
+
+dButton.addEventListener("click", function(){
+
+console.log("c");
+body.setAttribute("style", "background-color: blue");
+
+
+
+
+
+});
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
